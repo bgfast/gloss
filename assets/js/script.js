cd80807f6cf8511bbc088e073bdc99dc5b86e403
brentgr@microsoft.com <brentgr@microsoft.com>
2021-12-21 18:57:05 -0700

    Small start

--- assets/js/script.js
+++ assets/js/script.js
@@ -249,8 +249,11 @@ $(function () {
 				if(item.id == index){
 					// Populate '.preview-large' with the chosen product's data.
 					container.find('h3').text(item.name);
-					container.find('img').attr('src', item.image.large);
-					container.find('p').text(item.description);
+					//container.find('img').attr('src', item.image.large);
+					//container.getElementByClassName('sentence').text(item.sentence);
+					container.find('p.description').text(item.description);
+					container.find('p.sentence').text(item.sentence);
+					//container.find('sentence').text(item.sentence);
 				}
 			});
 		}
