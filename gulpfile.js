9a1d3fd16a9d51a76fa74596ce823e659640c34c
brentgr@microsoft.com <brent.groom@live.com>
2021-12-22 13:11:57 -0700

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
-gulp.task('default', function () {
-  console.log('Hello Gulp!')
+gulp.task('default', function (done) {
+  console.log('Hello Gulp!');
+  done();
 });
+
+
+// https://stackoverflow.com/questions/36897877/gulp-error-the-following-tasks-did-not-complete-did-you-forget-to-signal-async
